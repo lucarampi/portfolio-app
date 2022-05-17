5e3b11ff93a5e6932ea4d75f53fb80f059038bf8
Luca Alfaro Rampinelli <luca.alfaro.rampinelli@gmail.com>
2022-05-17 12:09:12 -0300

    responsivity and added buton for CV

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,15 +2,17 @@ import styles from "./styles.module.scss";
 import ActiveLink from "../ActiveLink";
 import TypeIt from "typeit-react";
 import { Background } from "../Background";
+import Link from "next/link";
+import { FilePdf } from "phosphor-react";
 export function Header() {
   return (
     <>
       <div className="absolute w-full h-screen overflow-hidden flex justify-center items-center">
         <Background />
-        <header className="absolute top-0 left-0 w-full h-full bg-white bg-opacity-[94%] mix-blend-screen">
-          <div className="flex flex-col h-full gap-5 my-0 mx-auto items-center justify-center max-w-[1020px]">
+        <header className="absolute px-2 top-0 left-0 w-full h-full bg-white bg-opacity-[94%] mix-blend-screen">
+          <div className="flex flex-col h-full gap-7 my-0 mx-auto items-center justify-center max-w-[1020px]">
             <TypeIt
-              className="text-black text-center text-9xl font-mono font-semibold antialiased bg-blend-screen"
+              className="text-black text-center font-mono font-black antialiased text-5xl sm:text-7xl md:text-8xl lg:text-10xl xl:text-12xl"
               options={{
                 lifeLike: true,
                 speed: 100,
@@ -43,14 +45,14 @@ export function Header() {
                 return instance;
               }}
             />
-            <nav className="flex ml-10 gap-16 text-xl text-black">
-              <ActiveLink
+            <nav className="flex px-5 flex-wrap justify-center items-center gap-x-10 gap-y-8 text-black text-xl sm:text-2xl md:text-2xl lg:text-3xl" >
+              {/* <ActiveLink
                 activeClassName="text-black"
                 defaultClassName="text-black-100"
                 href="/"
               >
                 <a>Home</a>
-              </ActiveLink>
+              </ActiveLink> */}
               <ActiveLink
                 activeClassName="text-black"
                 defaultClassName="text-black-100"
@@ -65,7 +67,14 @@ export function Header() {
               >
                 <a>Contact</a>
               </ActiveLink>
-              <p className="text-cyan-700">BOTÃO DE CONTATO</p>
+
+              <a
+                href="www.google.com"
+                target={"_blank"}
+                className=" py-2 px-5 w-fit bg-black text-white hover:text-black rounded-full ring-offset-white ring-offset-3 hover:ring-4 ring-black border-transparent flex justify-center items-center font-medium hover:bg-white focus:outline-none focus:ring-4 focus:ring-offset-4 focus:ring-offset-white focus:ring-black transition-all"
+              >
+                Curriculum
+              </a>
             </nav>
           </div>
         </header>
