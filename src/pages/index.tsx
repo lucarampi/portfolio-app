a242b68b4040ce5469488d1cabb2a61a18927594
Luca Alfaro Rampinelli <luca.alfaro.rampinelli@gmail.com>
2022-05-17 14:29:57 -0300

    home favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,15 @@
 /* eslint-disable @next/next/no-img-element */
 import type { NextPage } from "next";
 import Head from "next/head";
-import { Background } from "../components/Background";
 import { Header } from "../components/Header";
 
 const Home: NextPage = () => {
   return (
     <>
+    <Head>
+      <title>Luca A.R. | Home</title>
+      <link rel="shortcut icon" type="image/svg+xml" href="house.svg" />
+    </Head>
       <Header/>
     </>
   );
